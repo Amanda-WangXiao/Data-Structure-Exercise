cc53e23e3ef7959440efe623268f5ce2ae1d44b6
Amanda-WangXiao <amandawang0227@126.com>
2020-09-02 20:54:15 +0800

    update LinkedList

--- Java/LinkedList/LinkedListExercise.java
+++ Java/LinkedList/LinkedListExercise.java
@@ -5,10 +5,26 @@ class LinkedListExercise {
 	public static void main(String[] args)   {
 		LinkedListManager l = new LinkedListManager();
 		MyNode n1=new MyNode(1,"Alice",25);
+		MyNode n2=new MyNode(2,"Bob",20);
+		MyNode n3=new MyNode(3,"Charlie",29);
+		MyNode n4=new MyNode(4,"David",23);
 		l.add(n1);
+		l.add(n2);
+		l.add(n3);
+		l.add(n4);
+		
 		System.out.println("Original LinkedList: ");
 		l.list();
-	
+		System.out.println(" ");
+		
+		System.out.println("The LinkedList After Deleting the 3rd node: ");
+		l.delete(3);
+		l.list();
+		System.out.println(" ");
+		
+		System.out.println("The LinkedList After insert the 3rd node: ");
+		l.insert(n3);
+		l.list();
 	}
 }
 
@@ -16,25 +32,46 @@ class LinkedListExercise {
 class LinkedListManager{
 	MyNode headnode=new MyNode();	// Define a head node
 	
+	// Add a new node to the tail
 	public void add(MyNode node){
 		MyNode temp=headnode;
-		//if(headnode.next==null) System.out.println("This is an empty LinkedList");
 		while(true){
-			if(temp.next==null){
-				temp.next=node;
-			}
+			if(temp.next==null)	break;
 			temp=temp.next;
 		}
+		temp.next=node;
 	}
 	
+	//	Insert a new node
+	public void insert(MyNode node){
+		MyNode temp=headnode;
+		while(true){
+			if(temp.next.num>node.num) break;
+			else if(temp.next==null) System.out.println("Can not find the target position");
+			temp=temp.next;
+		}
+		node.next=temp.next;
+		temp.next=node;
+	}
+	
+	//	Delete a node in any position
+	public void delete(int mark){
+		MyNode temp=headnode;
+		while(true){
+			if(temp.next.num==mark) break;
+			else if(temp.next==null) System.out.println("Error: No such node");
+			temp=temp.next;
+		}
+		temp.next=temp.next.next;
+	}
+	
+	//	Go through the LinkedList
 	public void list(){
 		MyNode temp=headnode.next;
 		if(headnode.next==null) System.out.println("This is an empty LinkedList");
 		while(true){
 			System.out.println(temp);
-			if(temp.next==null){
-				break;
-			}
+			if(temp.next==null)	break;
 			temp=temp.next;
 		}
 	}
