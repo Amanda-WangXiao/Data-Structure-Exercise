eef0edacca4040f2db16c7a9b3fa7ea441c4aaa9
Amanda-WangXiao <amandawang0227@126.com>
2020-10-02 19:36:53 +0800

    update Stack

---
+++ Java/Stack/StackExercise.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import javax.xml.transform.*;
+
+class Exercise {
+	public static void main(String[] args)   {
+	
+		
+}
+}
+
+public class MyStack {
+     int top=-1;
+	 
+ }
